8de6d53571989770bb37a766ea41db949dd2b0f0
vetochkat <violetta007@me.com>
2024-01-18 19:32:18 +0300

    о

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+    );
+  }
+}
 class ProfilePage extends StatefulWidget {
   ProfilePage({Key? key}) : super(key: key);
 
@@ -15,18 +24,32 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-            child: const Text(
-          'UNIhelp',
-          style: TextStyle(fontSize: 30, fontFamily: 'Ubuntu'),
-        )),
+        title: Center(child: const Text(
+          'UNIhelp', style: TextStyle(fontSize: 30, fontFamily: 'Ubuntu'),)),
         backgroundColor: Color(0xff9fa8da),
       ),
-
-
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[300],
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Dialogues',
+          ),
+        ],
+      ),
       body: Column(
         children: [
           ProfiPic(),
+          UserName(),
+          Userid(),
         ],
       ),
     );
@@ -35,11 +58,20 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget ProfiPic() {
     return Container(
       alignment: Alignment.center,
-      child: Image.network(
-        'https://cs12.pikabu.ru/post_img/big/2022/03/21/10/164788475811377451.jpg',
-        height: 200,
-        width: 200,
-      ),
+      child: Image.network('https://cs12.pikabu.ru/post_img/big/2022/03/21/10/164788475811377451.jpg',
+        height: 150, width: 200, ),
+    );
+  }
+  Widget UserName(){
+    return Container(
+      alignment: Alignment.center,
+      child: Text('Имя Фамилия', style: TextStyle(fontSize: 28),),
+    );
+  }
+  Widget Userid(){
+    return Container(
+      alignment: Alignment.center,
+      child: Text('@userID1', style: TextStyle(fontSize: 20),),
     );
   }
 }
